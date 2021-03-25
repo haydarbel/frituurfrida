48aeaf240d88d85416502ca60ea9118801b03a27
haydar koktas <haydarkoktas@gmail.com>
2021-03-25 01:04:33 +0100

    post request is toegevogd

--- src/main/java/be/vdab/frituurfrida/controllers/SnackController.java
+++ src/main/java/be/vdab/frituurfrida/controllers/SnackController.java
@@ -1,13 +1,17 @@
 package be.vdab.frituurfrida.controllers;
 
 import be.vdab.frituurfrida.domain.BeginLetters;
+import be.vdab.frituurfrida.domain.Snack;
+import be.vdab.frituurfrida.exceptions.SnackNietGevondenException;
 import be.vdab.frituurfrida.services.SnackService;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.validation.Valid;
 
@@ -47,5 +51,27 @@ class SnackController {
         }
         return new ModelAndView("beginnaam", "snacks",
                 snackService.findByBeginNaam(form.getLetters()));
+    }
+
+    @GetMapping("{id}/wijzigen/form")
+    public ModelAndView wijzigenForm(@PathVariable long id) {
+        var modelAndView =  new ModelAndView("wijzigSnack");
+        snackService.read(id).ifPresent(snack ->
+                modelAndView.addObject("snack",snack));
+             return modelAndView;
+    }
+
+    @PostMapping("wijzigen")
+    public String wijzigen(@Valid Snack snack, Errors error, RedirectAttributes redirect) {
+        if (error.hasErrors()) {
+            return "wijzigSnack";
+        }
+        try {
+            snackService.update(snack);
+            return "redirect:/";
+        } catch (SnackNietGevondenException e) {
+            redirect.addAttribute("SnackNietGevonden", snack.getId());
+            return "redirect:/";
+        }
     }
 }
